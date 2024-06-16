a8c9417940fa83346215a0ef752d9fc95b3abac1
Caleb <chasch96@gmail.com>
2024-06-16 22:50:13 +0800

    create modalWithForm

---
+++ wtwr/src/components/App/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,46 @@
+export default function ModalWithForm() {
+  return (
+    <div className="modal" id="add-card-modal">
+      <form className="modal__form form" id="form-add-card">
+        <button
+          className="form__close-button modal__close"
+          type="button"
+          aria-label="close"
+        ></button>
+        <h2 className="form__title">New Garment</h2>
+        <fieldset className="form__set">
+          <label className="form__field">
+            <input
+              type="text"
+              className="form__input"
+              id="form__title"
+              name="title"
+              placeholder="Title"
+              required
+              maxLength="30"
+            />
+            <span className="form__error"></span>
+          </label>
+          <label className="form__field">
+            <input
+              type="url"
+              className="form__input"
+              id="form__image-link"
+              name="link"
+              placeholder="Image link"
+              required
+            />
+            <span className="form__error"></span>
+          </label>
+        </fieldset>
+        <button
+          type="submit"
+          className="form__submit-button form__submit-button_inactive"
+          disabled
+        >
+          Create
+        </button>
+      </form>
+    </div>
+  );
+}
